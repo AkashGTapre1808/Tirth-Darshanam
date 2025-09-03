fda909ab41e8cd7a7ce5609dc7be5d55e54b7429
Hamraj-Chavan <harryyy7797@gmail.com>
2025-09-03 22:22:02 +0530

    Create Chatbot.js

---
+++ .vscode/Chatbot.js
@@ -0,0 +1,28 @@
+function sendMessage() {
+  const input=document.getElementById("user-input");
+  const message=input.value.trim();
+
+  if(message==="") return;
+  addMessage(message, "user");
+  input.value="";
+
+  setTimeout(()=> {
+    const reply= getBotresponse(message);
+    addMessage(reply,"bot");
+  }, 1000); //1sec delay
+}
+function getBotResponse(input) {
+  input=input.toLowerCase();
+  if(input.includes("hello") || input.includes("hi")){
+    return "Hello, How can I help you?";
+  }
+    elseif (input.includes("how are you")){
+      return "i'm doing great ,What about You?"
+    }
+    elseif (input.includes("bye"){
+      return "bye! Have a goog day"
+    }
+    else{
+      return "Sorry, I don't Understand That. you Can ask any other question"
+    }
+}
